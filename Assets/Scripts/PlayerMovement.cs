cef3cbf576fe72f1cf19ef6595230daf2fc86cfa
Mateo Gonz <0262939@up.edu.mx>
2026-09-22 18:46:09 -0600

    Added left movement, jumping and platforms

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,53 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 10f;
+
+    Rigidbody2D rb;
+    bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
+        float move = 0;
+
+        // move left while holding A or the left arrow
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            move = -1;
+        }
+
         // move right while holding D or the right arrow
         if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            move = 1;
         }
+
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        // jump with space, only if we are standing on something
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        // only count it as ground if we are on top of it (not touching the side)
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
